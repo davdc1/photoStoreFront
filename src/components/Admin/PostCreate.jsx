d45c6833699d2707cdebb0ebb0a4725fa0f398ff
David Cohen <davdc1@gmail.com>
2021-10-25 18:37:22 +0300

    update

--- src/components/Admin/PostCreate.jsx
+++ src/components/Admin/PostCreate.jsx
@@ -1,10 +1,25 @@
 
 
-import { Create, SimpleForm, TextInput, DateInput } from 'react-admin'
+import { Create, SimpleForm, TextInput, DateInput, useNotify, useRefresh, useRedirect } from 'react-admin'
 
 export const PostCreate = (props) => {
+const notify = useNotify();
+const refresh = useRefresh();
+const redirect = useRedirect();
+
+let onFailure = (errors) => {
+    notify(`is that an error?`)
+    console.log('errors::::', errors);
+}
+
+let onSuccess = (res) => {
+    console.log('success!');
+    notify('success!')
+    //redirect('/posts');
+    //refresh();
+}
     return(
-        <Create title="Create a post" {...props} >
+        <Create onFailure={onFailure} onSuccess={onSuccess} title="Create a post" {...props} >
             <SimpleForm>
                 <TextInput source='userId' />
                 <TextInput source='postId' />
@@ -15,3 +30,4 @@ export const PostCreate = (props) => {
         </Create>
     )
 }
+

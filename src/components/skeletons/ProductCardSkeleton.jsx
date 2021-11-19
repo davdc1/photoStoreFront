9c41916aefcb06dfb954308a317c88bf0831b441
David Cohen <davdc1@gmail.com>
2021-11-19 03:12:21 +0200

    product-card skeleton update

--- src/components/skeletons/ProductCardSkeleton.jsx
+++ src/components/skeletons/ProductCardSkeleton.jsx
@@ -1,7 +1,7 @@
 
 export const ProductCardSkeleton = () => {
     return(
-        <div className='w-96 h-96 my-14 mx-4 border-2 border-light rounded flex justify-center items-center'>
+        <div className='w-screen sm:w-96 h-96 sm:h-96 my-14 mx-2 sm:mx-8 border-2 border-light rounded flex justify-center items-center'>
             <p>Loading...</p>
         </div>
     )
